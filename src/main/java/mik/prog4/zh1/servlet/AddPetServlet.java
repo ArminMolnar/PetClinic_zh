fa3c3a1dc4d17fefaad3e5affba75d4eed64d5c4
ArminMolnar <molnar.armin9@gmail.com>
2025-03-29 13:15:40 +0100

    use PetService instead of PetRepository

--- src/main/java/mik/prog4/zh1/servlet/AddPetServlet.java
+++ src/main/java/mik/prog4/zh1/servlet/AddPetServlet.java
@@ -5,13 +5,13 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import mik.prog4.zh1.entity.Pet;
-import mik.prog4.zh1.repository.PetRepository;
+import mik.prog4.zh1.service.PetService;
 
 import java.io.IOException;
 
 public class AddPetServlet extends HttpServlet {
 
-    private static final PetRepository petRepository = PetRepository.getInstance();
+    private static final PetService petService = new PetService();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
@@ -26,7 +26,7 @@ public class AddPetServlet extends HttpServlet {
         var chipCode = req.getParameter("chipCode");
         var address = req.getParameter("address");
         var species = req.getParameter("species");
-        petRepository.save(new Pet(name, chipCode, address, species));
+        petService.save(new Pet(name, chipCode, address, species));
         resp.sendRedirect( req.getContextPath() + "/listServlet");
     }
 }
--- src/main/java/mik/prog4/zh1/servlet/ListServlet.java
+++ src/main/java/mik/prog4/zh1/servlet/ListServlet.java
@@ -4,15 +4,13 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import mik.prog4.zh1.repository.PetRepository;
 import mik.prog4.zh1.service.PetService;
 
 import java.io.IOException;
 
 public class ListServlet extends HttpServlet {
 
-    private static final PetRepository petRepository = PetRepository.getInstance();
-    private static PetService petService = new PetService();
+    private static final PetService petService = new PetService();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
@@ -25,7 +23,7 @@ public class ListServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
         req.setCharacterEncoding("UTF-8");
-        petRepository.delete(Long.parseLong(req.getParameter("petId")));
+        petService.deletePet(Long.parseLong(req.getParameter("petId")));
         req.setAttribute("petList", petService.findAll());
         req.getRequestDispatcher("petList.jsp").forward(req, resp);
     }
