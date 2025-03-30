795262d6c051884c2a22fe4815f2117b84808979
ArminMolnar <molnar.armin9@gmail.com>
2025-03-30 17:54:14 +0200

    create RequestListener class for logging

---
+++ src/main/java/mik/prog4/zh1/listener/RequestListener.java
@@ -0,0 +1,17 @@
+package mik.prog4.zh1.listener;
+
+import jakarta.servlet.ServletRequestEvent;
+import jakarta.servlet.ServletRequestListener;
+
+import java.util.logging.Logger;
+
+public class RequestListener implements ServletRequestListener {
+
+    private static Logger LOGGER = Logger.getLogger(RequestListener.class.getName());
+
+    @Override
+    public void requestInitialized(ServletRequestEvent servletRequestEvent) {
+        LOGGER.info("Source:" + servletRequestEvent.getSource());
+        LOGGER.info("Request: " + servletRequestEvent.getServletRequest());
+    }
+}
